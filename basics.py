172de2ddbbb63f096db7355df5355bc6c121d6cc
Ritika <salgaonkar.ritika@gmail.com>
2025-07-28 20:28:58 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,66 @@
+x, y= "hi ","hello"
+print(x + y)
+
+#python has dynamic typing- it can modify values for the same variable
+a=2
+print(a+a+a)
+a=a+a
+print(a+a)
+a=a+a
+print(a+a)
+
+#type function- shows data type
+print(type(a))
+a=20.01
+print(type(a))
+
+income=1000
+tax_rate=0.2
+taxes=income*tax_rate
+print("my tax is equal to",taxes) 
+
+#imp
+print("hi, \ni'm the one and only!")
+print("hi, \ti'm the one and only!")
+
+print(len("I am mad hungry"))
+
+#indexing- grabbing one character from a string
+string= "sub space"
+print(string)
+print(string[2])
+print(string[-1])
+print(string[-3])
+
+#slicing- grabbing more than one character(closing value is excluded)
+string= 'abcdefghijk'
+print(string[:5])
+print(string[2:])
+print(string[2:7])
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
